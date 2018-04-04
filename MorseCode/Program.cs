3f86dbb6db15e17c42fcd55c2fc6cfacb61a079e
Tiller <ctiller@proforma.com>
2018-04-04 12:39:00 -0400

    add morse code file to filepath, take in data, and split out into both letter and morse code

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace MorseCode
     {
         static void Main(string[] args)
         {
+            // Create dictionary with string and with char values
+            var morseMap = new Dictionary<char, string>();
+            const string FILE_PATH = "../../../assets/morse.csv";
+            // use stremreader to read in the file.
+            using (var reader = new StreamReader(FILE_PATH))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var line = reader.ReadLine().Split(',');
+                    Console.WriteLine($"{line[0]} , {line[1]}");
+                }
+            }
+            Console.ReadLine();
+            // Grab all of the data in the file.
+            // Append it into dictionary.
         }
     }
 }
